933fafb8083ed2da4d7958353840593e63ed3de8
iwangcx <398739275@qq.com>
2018-03-21 22:24:46 +0800

    熟悉下框架

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 // import HelloWorld from '@/components/HelloWorld'
-import home from '@/views/home'
+// import home from '@/views/home'
+import App from '@/App'
 
 Vue.use(Router)
 
@@ -9,8 +10,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: home
+      name: 'App',
+      component: App
     }
   ]
 })

1eadab937e708cfddb3abd9464aba5f56ec4ee08
iwangcx <398739275@qq.com>
2018-06-21 22:43:12 +0800

    login

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ const bodyParser = require('koa-bodyparser');
 const router = require('./server/routes/index');
 const jwt = require('koa-jwt');
 const secret = require('./server/config/secret.json');
-const err = require('./server/middleware/error');
+const err = require('./server/middleware/err.js');
 const app = new Koa();
-
+console.log(err);
 app.use(err());
 app.use(logger());
 app.use(bodyParser());

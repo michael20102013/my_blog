d7b29909a83100c794fb40fc211af21c5e7275dd
iwangcx <398739275@qq.com>
2018-10-23 23:40:32 +0800

    修改样式 Signed-off-by: iwangcx <398739275@qq.com>

--- src/views/welcome.vue
+++ src/views/welcome.vue
@@ -35,7 +35,6 @@
       -webkit-font-smoothing: antialiased;
       -moz-osx-font-smoothing: grayscale;
       color: #2c3e50;
-      /*margin-top: 60px;*/
       height: 100%;
       width: 100%;
       transition: all 0.8s linear;
